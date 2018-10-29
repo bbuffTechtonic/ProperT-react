ca25dff77580fc3234ebfb4d40d0dcbdb269e2df
Bill Buff <bill.buff@techtonic.com>
2018-10-29 13:59:45 -0600

    Setup main component skeletons

---
+++ src/components/AccountManagement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class AccountManagement extends React.Component {
+  render() {
+    return (
+      <section className="account-management">
+
+      </section>
+    );
+  }
+}
+
+export default AccountManagement;
---
+++ src/components/PropertyDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class PropertyDetails extends React.Component {
+  render() {
+    return (
+      <section className="property-details">
+      
+      </section>
+    );
+  }
+}
+
+export default PropertyDetails;
