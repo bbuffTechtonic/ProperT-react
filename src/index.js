17d4a5205ba436d63e4e9a6652028b27ef6c37c0
Bill Buff <bill.buff@techtonic.com>
2018-10-29 13:45:58 -0600

    Incorporate bootstrap dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Row, Col} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
