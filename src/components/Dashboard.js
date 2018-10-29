ce2b068c8bcddfda3f76300a05535136a6910bbc
Alexis Zambrano <alexis.zambrano@techtonic.com>
2018-10-29 13:40:31 -0600

    initial commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Dashboard extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+
+}
+
+export default Dashboard;
+
+//ReactDOM.render(<Dashboard />, document.getElementById('root'));
