7e899d4749b17560b94e4bdea98bc97e28e06805
Mollie Chacon <mollie.chacon@techtonic.com>
2018-10-30 13:12:18 -0600

    update airbnb eslint info

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Row, Col } from 'reactstrap';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Container, Row, Col} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+
 const store = require('store');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  React.createElement(App), document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
