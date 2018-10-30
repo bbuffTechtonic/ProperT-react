163edc26c5e641e07ed4ffd2b902e2cb5b4ed115
Bill Buff <bill.buff@techtonic.com>
2018-10-30 13:28:40 -0600

    Add Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import AddProperty from './components/AddProperty';
+import AccountManagement from './components/AccountManagement';
+import PropertyDetails from './components/PropertyDetails';
 import './App.css';
 
 class App extends Component {
@@ -8,17 +13,36 @@ class App extends Component {
       fiscalYear: Date.now()
     };
   }
-  
+
   render() {
     return (
       <div className="App">
-        <header>
-          <nav>
-          
-          </nav>
-        </header>
+        <Router>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/dashboard">Dashboard</Link>
+                </li>
+                <li>
+                  <Link to="/add-property">Add Property</Link>
+                </li>
+                <li>
+                  <Link to="/account">My Account</Link>
+                </li>
+                <li>
+                  <Link to="/property-details">Property Details</Link>
+                </li>
+              </ul>
+            </nav>
+            <Route path="/dashboard" component={Dashboard}></Route>
+            <Route path="/add-property" component={AddProperty}></Route>
+            <Route path="/my-account" component={AccountManagement}></Route>
+            <Route path="/property-details" component={PropertyDetails}></Route>
+          </header>
+        </Router>
         <main>
-          <h1>Hello World</h1>
+          
         </main>
       </div>
     );
