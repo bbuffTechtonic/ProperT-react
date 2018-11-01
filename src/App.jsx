9d992d6fb05e81d227725a0db058ac7e4f941671
Sonali Marlin <sonali.marlin@techtonic.com>
2018-11-01 15:11:39 -0600

    change setState from store and remove props

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import './App.css';
 const store = require('store');
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     const date = new Date(Date.now());
     const currentYear = date.getFullYear();
     this.state = {
@@ -46,118 +46,209 @@ class App extends Component {
   }
 
   hydrateStateWithStore() {
-    // var address = {
-    //   address1: '123 Elm St.',
-    //   address2: '#305',
-    //   city: 'Denver',
-    //   state: 'CO',
-    //   zip: 80209
-    // };
-    //
-    // var tenantInfo = {
-    //   firstName: "Maggie",
-    //   lastName: "Brown",
-    //   email: "maggie@abc.org",
-    //   monthlyRent: 1750,
-    //   leaseStart: "6/1/2018",
-    //   leaseEnd: "5/31/2019"
-    // };
-    // //
-    // // //Expense Object
-    // var expenses = {
-    //   mileage: [
-    //     {
-    //       date: "7/5/2018",
-    //       amount: 25,
-    //       description: "Miles driven"
-    //     },
-    //     {
-    //       date: "8/18/2018",
-    //       amount: 25,
-    //       description: "Miles driven"
-    //     }
-    //   ],
-    //   mortgage: {
-    //     premium: 1700,
-    //     escrow: 800,
-    //     interest: 85,
-    //     mortgageIns: 100
-    //   },
-    //   maintenance: [
-    //     {
-    //       date: "7/18/2018",
-    //       description:"cleaning",
-    //       amount:160,
-    //       image:"../images/receipt.jpg"
-    //     }
-    //   ],
-    //   suppliesGoods: [
-    //     {
-    //       date: "9/01/2018",
-    //       description:"bathroom cleaning supplies",
-    //       amount:25,
-    //       image:"../images/receipt.jpg"
-    //     }
-    //   ],
-    //   misc: [
-    //     {
-    //       date: "7/18/2018",
-    //       description:"painting living room wall",
-    //       amount:800,
-    //       image:"../images/receipt.jpg"
-    //     }
-    //   ],
-    //   hoa: [
-    //     {
-    //       date: "6/1/2018",
-    //       amount: 50
-    //     }
-    //   ],
-    //   rent: [
-    //     {
-    //       date: "6/5/2018",
-    //       amount: 1750
-    //     },
-    //     {
-    //       date: "7/2/2018",
-    //       amount: 1750
-    //     },
-    //     {
-    //       date: "8/5/2018",
-    //       amount: 1750
-    //     }
-    //   ]
-    // };
-    // var newProp = {address, tenantInfo, expenses};
-    // var rentals = [];
-    // rentals.push(newProp);
-    // store.set('rentals', rentals);
-    // // for all items in state
+    var address = {
+      address1: '123 Elm St.',
+      address2: '#305',
+      city: 'Denver',
+      state: 'CO',
+      zip: 80209
+    };
 
-    for (let key in this.state) {
-      // if the key exists in localStorage
-      // if (store.key) {
-        // get the key's value from localStorage
-        let value = store.get(key) || "";
-        // console.log(value, 'value from localstorage');
-        // parse the localStorage string and setState
-        try {
-          value = JSON.parse(value);
-          this.setState({ [key]: value });
-        } catch (e) {
-          // handle empty string
-          this.setState({ [key]: value });
+    var tenantInfo = {
+      firstName: "Maggie",
+      lastName: "Brown",
+      email: "maggie@abc.org",
+      monthlyRent: 1750,
+      leaseStart: "6/1/2018",
+      leaseEnd: "5/31/2019"
+    };
+    //
+    // //Expense Object
+    var expenses = {
+      mileage: [
+        {
+          date: "7/5/2018",
+          amount: 25,
+          description: "Miles driven"
+        },
+        {
+          date: "8/18/2018",
+          amount: 25,
+          description: "Miles driven"
         }
-      // }
+      ],
+      mortgage: {
+        premium: 1700,
+        escrow: 800,
+        interest: 85,
+        mortgageIns: 100
+      },
+      maintenance: [
+        {
+          date: "7/18/2018",
+          description:"cleaning",
+          amount:160,
+          image:"../images/receipt.jpg"
+        }
+      ],
+      suppliesGoods: [
+        {
+          date: "9/01/2018",
+          description:"bathroom cleaning supplies",
+          amount:25,
+          image:"../images/receipt.jpg"
+        }
+      ],
+      misc: [
+        {
+          date: "7/18/2018",
+          description:"painting living room wall",
+          amount:800,
+          image:"../images/receipt.jpg"
+        }
+      ],
+      hoa: [
+        {
+          date: "6/1/2018",
+          amount: 50
+        }
+      ],
+      rent: [
+        {
+          date: "6/5/2018",
+          amount: 1750
+        },
+        {
+          date: "7/2/2018",
+          amount: 1750
+        },
+        {
+          date: "8/5/2018",
+          amount: 1750
+        }
+      ]
+    };
+
+    const Prop2 = {
+      address:
+      {
+      address1: '800 Water St.',
+      address2: '2B',
+      city: 'New York',
+      state: 'NY',
+      zip: 10410
+      },
+      tenantInfo: {
+      firstName: "Celeste",
+      lastName: "Green",
+      email: "celeste@def.org",
+      monthlyRent: 4850,
+      leaseStart: "4/1/2018",
+      leaseEnd: "5/31/2019"
+      },
+      expenses: {
+      mileage: [
+        {
+          date: "7/5/2018",
+          amount: 25,
+          description: "Miles driven"
+        },
+        {
+          date: "8/18/2018",
+          amount: 25,
+          description: "Miles driven"
+        },
+        {
+          date: "9/18/2018",
+          amount: 50,
+          description: "Miles driven"
+        }
+      ],
+      mortgage: {
+        premium: 3500,
+        escrow: 800,
+        interest: 85,
+        mortgageIns: 100
+      },
+      maintenance: [
+        {
+          date: "7/18/2018",
+          description:"cleaning",
+          amount:160,
+          image:"../images/receipt.jpg"
+        },
+        {
+          date: "8/18/2018",
+          description:"cleaning",
+          amount:260,
+          image:"../images/receipt.jpg"
+        }
+      ],
+      suppliesGoods: [
+        {
+          date: "9/01/2018",
+          description:"bathroom cleaning supplies",
+          amount:25,
+          image:"../images/receipt.jpg"
+        },
+        {
+          date: "10/01/2018",
+          description:"bathroom cleaning supplies",
+          amount:25,
+          image:"../images/receipt.jpg"
+        }
+      ],
+      misc: [
+        {
+          date: "7/18/2018",
+          description:"painting living room wall",
+          amount:800,
+          image:"../images/receipt.jpg"
+        }
+      ],
+      hoa: [
+        {
+          date: "6/1/2018",
+          amount: 50
+        },
+        {
+          date: "7/1/2018",
+          amount: 50
+        }
+      ],
+      rent: [
+        {
+          date: "6/5/2018",
+          amount: 4850
+        },
+        {
+          date: "7/2/2018",
+          amount: 4850
+        },
+        {
+          date: "8/5/2018",
+          amount: 4850
+        },
+        {
+          date: "9/5/2018",
+          amount: 4850
+        }
+      ]
     }
+  };
+  var newProp = {address, tenantInfo, expenses};
+  var rentals = [];
+  rentals.push(newProp);
+  rentals.push(Prop2);
+  store.set('rentals', JSON.stringify(rentals));
+
+  let value = JSON.parse(store.get('rentals'));
+  this.setState({'rentals': value });
   }
 
   saveStateToStore() {
     // for every item in React state
-    for (let key in this.state) {
-      // save to localStorage
-      store.set(key, JSON.stringify(this.state[key]));
-    }
+      store.set('rentals', JSON.stringify(this.state.rentals));
   }
 
   // updateInput(key, value) {
