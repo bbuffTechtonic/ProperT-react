aa8a2faf34ec6b0f529e5ee54cc0e07fdea02d85
Bill Buff <bill.buff@techtonic.com>
2018-11-02 14:55:02 -0600

    Change expense state by clicking nav

--- src/components/expenses/ExpenseDetails.jsx
+++ src/components/expenses/ExpenseDetails.jsx
@@ -4,36 +4,66 @@ import TableNav from './TableNav';
 import DisplayTable from './Table';
 import AddExpense from './AddExpense';
 
+const store = require('store');
+
 class ExpenseDetails extends Component {
   constructor(props) {
     super(props);
+    const { rental } = this.props;
     this.state = {
       modal: false,
-      navSelect: "All",
+      currentExpenses: rental.expenses,
     };
-
-    this.handleToggle = this.handleToggle.bind(this);
   }
 
-  handleToggle() {
+  handleToggle = () => {
     const { modal } = this.state;
     this.setState({ modal: !modal });
   }
 
-  // handleClick(e) {
-    
-  // }
+  handleSelectedExpense = (e) => {
+    const selectedExpense = e.target.textContent === "Supplies/Goods" ? "suppliesGoods" : e.target.textContent.toLowerCase();
+    const { rental } = this.props;
+    let expenses;
+    switch(selectedExpense) {
+      case "all":
+        expenses = rental.expenses;
+        break;
+      case "hoa":
+        expenses = rental.expenses.hoa;
+        break;
+      case "maintenance":
+        expenses = rental.expenses.maintenance;
+        break;
+      case "mileage":
+        expenses = rental.expenses.mileage;
+        break;
+      case "misc":
+        expenses = rental.expenses.misc;
+        break;
+      case "mortgage":
+        expenses = rental.expenses.mortgage;
+        break;
+      case "rent":
+        expenses = rental.expenses.rent;
+        break;
+      case "suppliesGoods":
+        expenses = rental.expenses.suppliesGoods;
+        break;
+      }
+    this.setState({ currentExpenses: expenses });
+  }
 
   render() {
-    const { modal, navSelect } = this.state;
+    const { modal, currentExpenses } = this.state;
     const { rental } = this.props;
     return (
       <section id="expense-details">
         <TableButtons toggle={this.handleToggle} isOpen={modal} />
         <AddExpense toggle={this.handleToggle} isOpen={modal} />
         <div className="row">
-          <TableNav handleClick={this.handleClick} />
-          <DisplayTable expenses={rental.expenses} navSelect={navSelect} />
+          <TableNav selectExpense={this.handleSelectedExpense} />
+          <DisplayTable currentExpenses={currentExpenses} createTable={this.createTable} />
         </div>
       </section>
     );
--- src/components/expenses/TableNav.jsx
+++ src/components/expenses/TableNav.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-function TableNav() {
+function TableNav({ selectExpense }) {
   return (
     <div className="col-md-3">
       <div className="card">
         <div className="card-body" id="exp-nav-card">
           <nav className="nav flex-column nav-pills" id="v-pills-tab">
-            <a className="nav-link active bg-success" id="v-pills-all-tab" data-toggle="pill" href="#v-pills-all" role="tab" aria-controls="v-pills-all" aria-selected="true">All</a>
-            <a className="nav-link text-success" id="v-pills-mileage-tab" data-toggle="pill" href="#v-pills-mileage" role="tab" aria-controls="v-pills-mileage" aria-selected="false">Mileage</a>
-            <a className="nav-link text-success" id="v-pills-mortgage-tab" data-toggle="pill" href="#v-pills-mortgage" role="tab" aria-controls="v-pills-mortgage" aria-selected="false">Mortgage</a>
-            <a className="nav-link text-success" id="v-pills-maintenance-tab" data-toggle="pill" href="#v-pills-maintenance" role="tab" aria-controls="v-pills-maintenance" aria-selected="false">Maintenance</a>
-            <a className="nav-link text-success" id="v-pills-supplies/goods-tab" data-toggle="pill" href="#v-pills-supplies/goods" role="tab" aria-controls="v-pills-supplies/goods" aria-selected="false">Supplies/Goods</a>
-            <a className="nav-link text-success" id="v-pills-misc-tab" data-toggle="pill" href="#v-pills-misc" role="tab" aria-controls="v-pills-misc" aria-selected="false">Misc</a>
-            <a className="nav-link text-success" id="v-pills-rent-tab" data-toggle="pill" href="#v-pills-rent" role="tab" aria-controls="v-pills-rent" aria-selected="false">Rent</a>
+            <a onClick={selectExpense} className="nav-link active bg-success" data-toggle="pill" href="#v-pills-all" role="tab" aria-controls="v-pills-all" aria-selected="true">All</a>
+            <a onClick={selectExpense} className="nav-link text-success" data-toggle="pill" href="#v-pills-mileage" role="tab" aria-controls="v-pills-mileage" aria-selected="false">Mileage</a>
+            <a onClick={selectExpense} className="nav-link text-success" data-toggle="pill" href="#v-pills-mortgage" role="tab" aria-controls="v-pills-mortgage" aria-selected="false">Mortgage</a>
+            <a onClick={selectExpense} className="nav-link text-success" data-toggle="pill" href="#v-pills-maintenance" role="tab" aria-controls="v-pills-maintenance" aria-selected="false">Maintenance</a>
+            <a onClick={selectExpense} className="nav-link text-success" data-toggle="pill" href="#v-pills-supplies/goods" role="tab" aria-controls="v-pills-supplies/goods" aria-selected="false">Supplies/Goods</a>
+            <a onClick={selectExpense} className="nav-link text-success" data-toggle="pill" href="#v-pills-misc" role="tab" aria-controls="v-pills-misc" aria-selected="false">Misc</a>
+            <a onClick={selectExpense} className="nav-link text-success" data-toggle="pill" href="#v-pills-rent" role="tab" aria-controls="v-pills-rent" aria-selected="false">Rent</a>
           </nav>
         </div>
       </div>
