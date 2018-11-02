c6b12e0c9b00f58db6d4d400be878ab113637768
Alexis Zambrano <alexis.zambrano@techtonic.com>
2018-11-02 14:30:44 -0600

    Dashboard with Rentals Storage data binding

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -13,6 +13,7 @@ class Dashboard extends React.Component {
     this.handleClick = this.handleClick.bind(this);
     this.state = {
       rentals: [],
+      optYears: [],
       fiscalYear: currentYear,
       totExpenses: '10,000.00',
       totIncome: '20,000.00',
@@ -23,11 +24,61 @@ class Dashboard extends React.Component {
   async componentDidMount() {
     const value = JSON.parse(store.get('rentals'));
     await this.setState({ rentals: value });
+    this.setState({ optYears: this.getYears() })
     this.getYearTotals(this.state.fiscalYear);
   }
 
-  componentDidUpdate() {
-    this.getYearTotals(this.state.fiscalYear);
+  getYears() {
+
+    let distinctYears = [];
+    let oYear;
+
+    for (let i = 0;i < this.state.rentals.length;i++) {
+      for (let j = 0;j < this.state.rentals[i].expenses.hoa.length;j++) {
+        oYear = new Date(this.state.rentals[i].expenses.hoa[j].date)
+        distinctYears.push(oYear.getFullYear());
+      }
+    }
+
+    for (let i = 0;i < this.state.rentals.length;i++) {
+      for (let j = 0;j < this.state.rentals[i].expenses.maintenance.length;j++) {
+        oYear = new Date(this.state.rentals[i].expenses.maintenance[j].date)
+        distinctYears.push(oYear.getFullYear());
+      }
+    }
+
+    for (let i = 0;i < this.state.rentals.length;i++) {
+      for (let j = 0;j < this.state.rentals[i].expenses.misc.length;j++) {
+        oYear = new Date(this.state.rentals[i].expenses.misc[j].date)
+        distinctYears.push(oYear.getFullYear());
+      }
+    }
+
+    for (let i = 0;i < this.state.rentals.length;i++) {
+      for (let j = 0;j < this.state.rentals[i].expenses.suppliesGoods.length;j++) {
+        oYear = new Date(this.state.rentals[i].expenses.suppliesGoods[j].date)
+        distinctYears.push(oYear.getFullYear());
+      }
+    }
+
+    for (let i = 0;i < this.state.rentals.length;i++) {
+      for (let j = 0;j < this.state.rentals[i].expenses.rent.length;j++) {
+        oYear = new Date(this.state.rentals[i].expenses.rent[j].date)
+        distinctYears.push(oYear.getFullYear());
+      }
+    }
+
+    let uniqueArray = [];
+    let uniqueOptArray = [];
+    for(let i = 0;i < distinctYears.length; i++){
+      if(uniqueArray.indexOf(distinctYears[i]) == -1){
+          uniqueArray.push(distinctYears[i]);
+          uniqueOptArray.push(<option value= {distinctYears[i]} >{distinctYears[i]}</option>);
+      }
+    }
+
+    return uniqueOptArray;
+
   }
 
   getYearTotals(year) {
@@ -72,15 +123,6 @@ class Dashboard extends React.Component {
       }
     }
 
-    // for (let i = 0;i < this.state.rentals.length;i++) {
-    //   for (let j = 0;j < this.state.rentals[i].expenses.hoa.length;j++) {
-    //     oYear = new Date(this.state.rentals[i].expenses.hoa[j].date)
-    //     if (oYear.getFullYear() === year) {
-    //       totExpenses = totExpenses + this.state.rentals[i].expenses.hoa[j].amount;
-    //     }
-    //   }
-    // }
-
     for (let i = 0;i < this.state.rentals.length;i++) {
       for (let j = 0;j < this.state.rentals[i].expenses.rent.length;j++) {
         oYear = new Date(this.state.rentals[i].expenses.rent[j].date)
@@ -113,10 +155,7 @@ class Dashboard extends React.Component {
               <label className="input-group-text">Year</label>
             </div>
             <select onChange={this.handleClick} defaultValue={fiscalYear} className="custom-select" id="my-year">
-              <option value="2018">2018</option>
-              <option value="2017">2017</option>
-              <option value="2016">2016</option>
-              <option value="2015">2015</option>
+              {this.state.optYears}
             </select>
           </div>
         </div>
